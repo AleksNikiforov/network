8ea984f7f511e58e92a09be107512649aa837c3c
AleksNikiforov <35526161+AleksNikiforov@users.noreply.github.com>
2023-03-31 11:28:13 +0500

    Update prep-env.sh

--- prep-env.sh
+++ prep-env.sh
@@ -1,6 +1,5 @@
 #Preparation of environment:
 #!/bin/bash
-sudo apt install libmariadbclient-dev
 sudo apt install python3-pip
 sudo apt install python3-venv
 sudo apt install python3-dev 
@@ -8,9 +7,8 @@ sudo apt install zip
 python3 -m venv .env
 source .env/bin/activate
 pip install -r requirements.txt
-pip install scrapli[asyncssh]
 #Preparation google_ghrome and ghromedriver for selenium
-cd ~/scout-crawler/stuff/
+cd ~/stuff/
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/
